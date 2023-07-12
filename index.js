1acaabf1e5d8c3133b483029bbfbd42f197dd0cd
Nadia-fiaz <139068814+Nadia-fiaz@users.noreply.github.com>
2023-07-12 23:48:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hello World");
+var myname = "Nadia";// string datatype
+//let, constant, var -
+var age = 25; // number
+var adress = "rwp";
+console.log ("my name is ",myname);
+console.log ("age is ",age);
+alert("i am nadia");
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("Hellow World");
+let myname = 'Nadia'// string datatype
+//let, const, var -
+const age = 25; // number
+var adress = "rwp";
+
+console.log ("my name is ", myname);
+console.log ("age is ", age);
