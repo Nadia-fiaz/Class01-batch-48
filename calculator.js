d351f595e19d9e0c9980d178ee236ab120ebea20
Nadia Fayyaz <139068814+Nadia-fiaz@users.noreply.github.com>
2023-07-16 22:44:40 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+// arthmatic operators ,plus ,minus, multiplication and divis
+var n1 = 10;
+var n2 = 20;
+console.log(n1 + n2);
+var x = 3, y = 6;
+var Add, Sub, Mul, Div;
+Add = x + y;
+console.log("Addition of x and y = " + Add);
+Sub = x - y;
+console.log("\nSubtraction of x and y = " + Sub);
+Mul = x * y;
+console.log("\nMultiplication of x and y = " + Mul);
+Div = x / y;
+console.log("\nDivision of x and y = " + Div);
---
+++ calculator.ts
@@ -0,0 +1,19 @@
+
+// arthmatic operators ,plus ,minus, multiplication and divis
+let n1:number =10;
+let n2:number =20;
+console.log(n1+n2)
+
+let x:number=3,y:number=6;
+let Add,Sub,Mul,Div;
+Add = x+y;
+console.log("Addition of x and y = " + Add);
+
+Sub = x-y;
+console.log("\nSubtraction of x and y = " + Sub);
+
+Mul = x*y;
+console.log("\nMultiplication of x and y = " + Mul);
+
+Div = x/y;
+console.log("\nDivision of x and y = " + Div);
