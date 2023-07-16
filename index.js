523689b44f8dc64e97e72473e9cf304c411f0750
Nadia Fayyaz <139068814+Nadia-fiaz@users.noreply.github.com>
2023-07-16 19:16:11 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 "use strict";
-console.log("Hello World");
-var myname = "Nadia";// string datatype
-//let, constant, var -
-var age = 25; // number
+console.log("Hellow World");
+let myname = 'Nadia'; // string datatype
+//let, const, var -
+const age = 25; // number
 var adress = "rwp";
-console.log ("my name is ",myname);
-console.log ("age is ",age);
-alert("i am nadia");
+console.log("my name is ", myname);
+console.log("age is ", age);
+let myName = "nadia";
+// myname12515; // it' gives us error
+//let const var 
+const myCity = "isb";
+var $age = 25;
+if (true) {
+    let myName = "nadia";
+    //let const var 
+    const myCity = "isb";
+    var age1 = 25;
+    console.log(myname);
+    console.log(myCity);
+}
+//console.log (mycity);
+console.log(age);
+let myName1 = "nadia";
+console.log(myName);
--- index.ts
+++ index.ts
@@ -6,3 +6,33 @@ var adress = "rwp";
 
 console.log ("my name is ", myname);
 console.log ("age is ", age);
+
+let myName = "nadia";
+// myname12515; // it' gives us error
+
+
+//let const var 
+
+const myCity = "isb";
+
+var $age = 25;
+
+if (true){let myName = "nadia";
+
+//let const var 
+
+const myCity = "isb";
+
+var age1 = 25;
+console.log (myname);
+console.log (myCity);
+}
+//console.log (mycity);
+
+console.log (age);
+
+let myName1 = "nadia";
+console.log (myName);
+
+
+
