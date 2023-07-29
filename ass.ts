2326480acfed4073b6afa48558896304b7b01e48
Nadia Fayyaz <139068814+Nadia-fiaz@users.noreply.github.com>
2023-07-29 22:01:16 +0500

    Add files via upload

---
+++ ass.ts
@@ -0,0 +1,112 @@
+//Q1,	Personal Message: Store a person’s name in a variable, 
+//and print a message to that person. Your message should be simple,
+// such as, “Hello Eric, would you like to learn some Python today?”
+let person=("Hello Eric,would you like to learn some python today");
+console .log(person);
+
+//Q2 Name Cases: Store a person’s name in a variable, and then print 
+//that person’s name in lowercase, uppercase, and titlecase.
+let person_name= "nadia"
+
+console.log("Lowercase:",person_name.toLowerCase());
+console.log("Uppercase:",person_name.toUpperCase());
+
+//Q3Famous Quote: Find a quote from a famous person you admire. 
+//Print the quote and the name of its author. 
+//Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let naMe:string = "Albert Einstein";
+console.log(naMe +" once said,  "+ "A person who never made a mistake never tried anything new.");
+
+//Q4 	Famous Quote 2: Repeat Exercise 4, 
+//but this time store the famous person’s name in a variable called famous_person. 
+//Then compose your message and store it in a new variable called message. 
+//Print your message.
+let famous_person = "Mahatma Gandhi"
+let message = famous_person + " once said, \"Be the change that you wish to see in the world.\""
+console.log(message)
+
+	/* Q5 Stripping Names: Store a person’s name, 
+and include some whitespace characters at 
+the beginning and end of the name.
+ Make sure you use each character combination, 
+ "\t" and "\n", at least once. Print the name once,
+  so the whitespace around the name is displayed.
+   Then print the name after striping the white spaces.*/
+
+   let name_with_whitespace = "\t\n   nadia sheikh  \t\n"
+console.log("name with white space",name_with_whitespace);
+let name_stripped = name_with_whitespace.trim()
+
+console.log("name after stripping whitespace",name_stripped);
+
+/*Q6 Number Eight: Write addition, subtraction, multiplication, and division operations that each
+ result in the number 8. 
+Be sure to enclose your operations in print statements to see the results.*/
+
+let x: number=5
+let y: number=3
+console.log("Addition of x and y = ",x+y);
+let x1: number = 15
+let y1: number = 7
+console.log("subtraction of x1 and y1=",x1-y1);
+let x2: number = 2
+let y2: number = 4
+console.log("multiplication of x2 and y2=",x2*y2);
+let x3: number= 64
+let y3: number= 8
+console.log("division of x3 and y3 =",x3/y3);
+
+/* Q7 Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+create a message that reveals your favorite number. Print that message.*/
+
+let favoriteNumber= 6
+let Message="my favorite number is: "+ (favoriteNumber);
+console.log(Message);
+
+/*Q8:.Adding Comments: Choose two of the programs you’ve written, 
+and add at least one comment to each. If you don’t have anything 
+specific to write because your programs are too simple at this point, 
+just add your name and the current date at the top of each program file. 
+Then write one sentence describing what the program does.*/
+let myName = "NADIA FAYYAZ";
+//string single and double codes back slash and  concatinate with + nadia Fayyaz 29 july 23
+
+let pera = "my new project is web development ,"+ myName + " i am working on it .\n wish me a best of luck.";
+console.log(pera)
+// template literals. nadia fayyaz 29 july 2023
+let data = `my new project is web development,${myName}.i am working on it .wish me best of luck`;
+console.log(data)
+console.log(typeof data)
+
+/* Q9 11.	Names: Store the names of a few of your friends in a array called names.
+ Print each person’s name by accessing each element in the list, one at a time.*/
+ let Name = ['ayesha','sana','zeenat','hajra'];
+ console.log(Name)
+
+ /* Q10 12.	Greetings: Start with the array you used in Exercise 11,
+  but instead of just printing each person’s name, print a message to them. 
+  The text of each message should be the same, 
+ but each message should be personalized with the person’s name.*/
+ 
+function Greet(name : string){
+
+    console.log("Good Morning," + name . toUpperCase() + "!!");
+
+}
+Greet("ayesha");
+Greet("sana");
+// function with return 
+function Greet1(name1 : string){
+    return name1 .toUpperCase();
+}
+Greet("Najma");
+
+/*Think of your favorite mode of transportation, 
+such as a motorcycle or a car, and make a list that stores several examples. 
+Use your list to print a series of statements about these items, 
+such as “I would like to own a Honda motorcycle.”*/
+
+
+
--- index.js
+++ index.js
@@ -1,101 +1,47 @@
 "use strict";
-console.log("Hellow World");
-let myname = 'Nadia'; // string datatype
-//let, const, var -
-const age = 25; // number
-var adress = "rwp";
-console.log("my name is ", myname);
-console.log("age is ", age);
-let myName = "nadia";
-// myname12515; // it' gives us error
-//let const var 
-const myCity = "isb";
-var $age = 25;
-if (true) {
-    let myName = "nadia";
-    //let const var 
-    const myCity = "isb";
-    var age1 = 25;
-    console.log(myname);
-    console.log(myCity);
+const promptSync = require('prompt-sync')();
+let userName = promptSync("enter your name..");
+console.log("your name is", userName);
+// functions
+// types
+// 1: no input  no return
+// 2: pass input but no returns
+//3: pass input and get output
+let sum1 = (x, y) => { return x + y; };
+let result = sum1(10, 20);
+console.log(result);
+if ("assignmentDone == true") {
+    console.log("you can also come in class");
 }
-//console.log (mycity);
-console.log(age);
-let myName1 = "nadia";
-console.log(myName);
-// git hub extension.
-let git = ('reseporatory');
-console.log(git);
-var age2 = 42;
-console.log(age2);
-// arthmatic operators ,plus ,minus, multiplication and divide .
-let n1 = 10;
-let n2 = 20;
-console.log(n1 + n2);
-if (true) {
-    let myname = "Nadia";
-    let mycity = 'isb';
+else {
+    console.log("you can not come to class");
 }
-console.log(myname);
-console.log(myCity);
-let value; //undefine
-console.log("Value will be", value);
-let value2 = null; //undefine
-console.log("Value will be", value2);
-//How to initalize boolean
-let youHavePassport = false;
-// let passportNo :null;
-// passportNo ='PA23928382'
-// passportNo =null
-let para = "Copyright OpenJS Foundation  and Node.js contributors." +
-    myname +
-    " All rights reserved. The OpenJS Foundation has registered trademarks and uses trademarks. \nFor a list of trademarks of the OpenJS Foundation, please see our Trademark Policy and Trademark List";
-console.log(para);
-let line2 = "This is Usman's code";
-// ============================== template letrals
-let data = `Copy
-right
- O
- p
- e
- n
- JS
-  Foundation ${myname}
-and Node.js contributors.
-All rights reserved.`;
-console.log(data);
-console.log(typeof data);
-// Operators
-//additon +
-let num1 = 20;
-let num2 = 15;
-let reslut = num1 + num2;
-console.log("result will be", reslut);
-let values = "any value will be here";
-let values2 = "here is the value.....";
-console.log(values + values2);
-let nums1 = 20;
-let nums2 = 15;
-console.log(nums1 + nums2);
-nums2 = nums2 + 5;
-nums2 -= 5; //nums2 +5
-console.log(nums2);
-// ============================= comparison opertaots
-//let nums1 = 52;
-//let nums2 = 15;
-console.log(nums1 != nums2);
-//AND operator  &&
-//      // 52  > 15 (true) &&  52 > 30(false)
-console.log(num1 > num2 && num1 > 30);
-// 52  > 15 (true) &&  52 > 30(true)
-console.log(nums1 > nums2 && nums1 > 30);
-//OR  ||
-// ture             52 > 300 false
-console.log(nums1 < nums2 || nums1 > 300);
-//NOT  !
-//  52 < 300 true-> ! true = false
-console.log(!(nums1 < 300));
-// =================================
-let var1 = "23";
-let var2 = 20;
-let res = var1 + var2;
+let marks = 90;
+if (marks >= 90 && marks < 100) {
+    console.log("Grade A+");
+}
+//80-90 -> A
+else if (marks >= 80 && marks < 90) {
+    console.log("Grade A");
+}
+else if (marks >= 70 && marks < 80) {
+    console.log("Grade B");
+}
+else {
+    console.log("invalid");
+}
+// task . 3 input balance , min balance , max balance 
+function creditScore(balance, minBalance, maxBalance) {
+    balance = 340;
+    minBalance = 300;
+    maxBalance = 150;
+    if (marks >= 250 && marks <= 300)
+        ;
+}
+;
+// 
+let fruits = ["apple", "banana", 'peach', 'mango'];
+console.log('fruit'[3]);
+// arrays funtions: add remove
+// built in functions type script 
+arr2;
